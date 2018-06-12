9c56ae85393a582bed1ab59748ba08cb6caef43b
Victor Andres Caram Guimaraes <victor.caram@piracicaba.grad.dcc.ufmg.br>
2018-06-11 21:23:41 -0300

    Progredindo um pouco

--- router.py
+++ router.py
@@ -3,7 +3,139 @@ import sys
 import socket
 import json
 import time
-import _thread as thread
+import threading as th
+
+#--------------------Input usuario--------------------
+
+#comando add <ip> <weight>
+#periodo_tempo para excluir após 4pi
+def adicionaRoteador(vizinhos, comando, roteadores):
+    endereco = comando[1]
+    peso = int(comando[2])
+    tamLista = range(len(vizinhos))
+    for i in tamLista:
+        print(vizinhos[i])
+        if(vizinhos[i][0] == endereco):
+            if(vizinhos[i][1] > peso):
+                vizinhos[i][1] = peso
+            return
+    vizinhos.append([endereco, peso])
+    
+    roteadores = vizinhos
+    print('roteadores', roteadores)
+    # tamLista = range(len(roteadores))
+    # for i in tamLista:
+    #     print(roteadores[i])
+    #     if(roteadores[i][0] == endereco):
+    #         if(roteadores[i][1] > peso):
+    #             roteadores[i][1] = peso
+    #             # roteadores[i][2].append([endereco])
+    #         if(roteadores[i][1] == peso):
+    #             roteadores[i][2].append([endereco])
+    #         return
+    # roteadores.append([endereco, peso, [endereco]])
+    
+#comando del <ip>
+def deletaRoteador(vizinhos, comando):
+    ip_del = comando[1]
+    for item in vizinhos:
+         if item[0] == ip_del:
+             vizinhos.remove(item)
+
+def leInput(vizinhos, roteadores, addr, conn_udp):
+    while True:
+        comando = input()
+        comando = comando.split(' ')
+
+        if (comando[0] == "add"):
+            adicionaRoteador(vizinhos, comando, roteadores)
+            print('vizinho adicionado. Estado da tabela:')
+            print(vizinhos)
+            # print(roteadores)
+            continue
+        
+        if (comando[0] == "del"):
+            deletaRoteador(vizinhos, comando)
+            print('vizinho deletado. Estado da tabela: ')
+            print(vizinhos)
+            continue
+      
+        if(comando[0] == "trace"):
+            #criar funcao pra calcular trace
+            ipDestino = comando[1]
+            pacote = json.dumps({'type': 'trace', 'source': addr, 'destination': ipDestino, 'hops': [addr]})
+            if(pacote["source"] == pacote["destination"]):
+                print(pacote)
+                continue
+            
+            procuraCaminho(pacote, ipDestino, roteadores, vizinhos)
+            continue
+        
+        #só termina com Ctrl + c 
+        else: 
+            sys.exit() 
+
+
+#-------------------Trace--------------------------
+
+
+
+
+def caminhoDireto(ipDestino, roteadores):
+    tamLista = range(len(roteadores))
+    for i in tamLista:
+        if(roteadores[i][0] == ipDestino):
+            print("oi")
+
+
+#comando trace <ip>
+def procuraCaminho(pacote, ipDestino, roteadores, vizinhos):
+    #faz uma procura com os roteadores ligados DIRETAMENTE ao host
+    caminho = caminhoDireto(ipDestino, roteadores)
+    if(caminho[0] == True):
+        enviaPacote(caminho[1], pacote)
+
+    #faz uma procura com os roteadores ligados INDIRETAMENTE ao host
+    caminho = caminhoIndireto(ipDestino, vizinhos)
+    if(caminho[0] == True):
+        enviaPacote(caminho[1], pacote)
+
+    if(caminho[0] == False):
+        return
+
+
+
+#------------------Update--------------------------  
+#A CADA PI(constante durante a execução do roteador - PERIOD) SEGUNDOS ELE DEVE MANDAR MSG DE UPDATE. 
+#ai depois tem que ter um delay e volta a receber comandos
+# time.sleep(period)#delay
+
+#Mensagem: UPDATE
+#esse daqui manda um "distances" e vai atualizar o peso, mas aparentemente a gente tem que cuidar de "Atualizações periodicas" e "Split Horizon"
+
+
+#--------------------Send/Recive--------------------
+def recebePacote(udp, pacote): 
+    while True:
+        ip, msg = udp.recvfrom(1024)
+        print(msg)
+        print(ip)
+        if(msg != ""):
+            msg = bytes.decode(msg)
+            mensagem = json.loads(msg)
+            pacote.append(mensagem)
+
+
+    #****MANDA MENSAGEM******
+def enviaPacote(ip_destino, pacote):
+    # Manda sua tabela para os vizinhos
+    msg = json.dumps(pacote)
+    mensagem = bytes(msg, 'utf-8')
+    destino = (ip_destino, port)
+    conn_udp.sendto(mensagem, destino)
+
+
+#--------------------Main--------------------
 
 #/router.py <ADDR> <PERIOD> [STARTUP]
 # <PERIOD>: tempo para fazer o update
@@ -16,108 +148,23 @@ startup = sys.argv[3]
 
 #Listas
 roteadores = []
-lista_vizinhos = []
-
-#-------------------Trace--------------------------
-#payload = mensagem trace
-def criaMensagemData(ipDestino, ipOrigem, payload):
-    print(json.dumps({'type': 'data', 'source': ipOrigem, 'destination': ipDestino, 'payload': payload}, indent=2))
-
-#comando trace <ip>
-def criaRota(lista_vizinhos, comando, ipOrigem):
-    ipDestino = comando[1]
-    hops = [ipOrigem]
-    
-    #cria JSON de trace
-    msgTrace = json.dumps({'type': 'trace', 'source': ipOrigem, 'destination': ipDestino, 'hops': hops}, indent=2)
-    print(msgTrace)
-    #cria payload para usar em msgData
-    payload = json.dumps({'type': 'trace', 'source': ipOrigem, 'destination': ipDestino, 'hops': hops})
-    criaMensagemData(ipDestino, ipOrigem, payload)
+vizinhos = []
+pacote = []
 
 
-#------------------Update--------------------------  
-#A CADA PI(constante durante a execução do roteador - PERIOD) SEGUNDOS ELE DEVE MANDAR MSG DE UPDATE. 
-#ai depois tem que ter um delay e volta a receber comandos
-# time.sleep(period)#delay
+#isso daqui vai bindar o roteador com o IP referente a ele,  criado pelo script lá
+conn_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+origem = (addr, port)
+conn_udp.bind(origem)
 
-#Mensagem: UPDATE
-#esse daqui manda um "distances" e vai atualizar o peso, mas aparentemente a gente tem que cuidar de "Atualizações periodicas" e "Split Horizon"
+#thread1
+threadRecieve = th.Thread(target = recebePacote, args = (conn_udp, pacote))
+#recebePacote(pacote)
 
-#-----------------Thread e chamada de função--------
-# thread.start_new_thread(função, (conexão,)) 
+#thread2
+threadInput = th.Thread(target = leInput, args = (vizinhos, roteadores, addr, conn_udp))
+#leInput(vizinhos, roteadores, conn_udp)
 
-
-#--------------------Send/Recive--------------------
-def connection(lista_vizinhos): 
-
-    #isso daqui vai bindar o roteador com o IP referente a ele,  criado pelo script lá
-    conn_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    #****RECEBE MENSAGEM*****
-    origem = (addr, port)
-    conn_udp.bind(origem)
-    while True:
-        msg, vizinho = udp.recvfrom(1024)
-        print(msg)
-        print(vizinho)
-    conn_udp.close()
-
-
-    #****MANDA MENSAGEM******
-    # Manda sua tabela para os vizinhos
-    mensagem = []
-    for item in lista_vizinhos:
-        mensagem.append([item[0], item[1]])
-
-    for item in lista_vizinhos: 
-        ip_destino = item[0]
-        destino = (ip_destino, port)
-        conn_udp.sendto(mensagem, destino)
-    conn_udp.close()
-
-#--------------------Input usuario--------------------
-
-#comando add <ip> <weight>
-#periodo_tempo para excluir após 4pi
-def adicionaRoteador(tabela, host, custo, salto, periodo_tempo):
-    tabela.append([host, custo, salto, periodo_tempo])
-  
-#comando del <ip>
-def deletaRoteador(lista_vizinhos, comando):
-    ip_del = comando[1]
-    for item in lista_vizinhos:
-         if item[0] == ip_del:
-             lista_vizinhos.remove(item)
-
-while True:
-    comando = input()
-    comando = comando.split(' ')
-
-    if (comando[0] == "add"):
-        host = comando[1]
-        custo = comando[2]
-        salto = host
-        periodo_tempo = 0
-        adicionaRoteador(lista_vizinhos, host, custo, salto, periodo_tempo)
-        print('vizinho adicionado. Estado da tabela:')
-        print(lista_vizinhos)
-        continue
-    
-    if (comando[0] == "del"):
-        deletaRoteador(lista_vizinhos, comando)
-        print('vizinho deletado. Estado da tabela: ')
-        print(lista_vizinhos)
-        continue
-  
-    if(comando[0] == "trace"):
-        #criar funcao pra calcular trace
-        criaRota(lista_vizinhos, comando, addr)
-        print("trace")
-        continue
-    
-    #só termina com Ctrl + c 
-    else: 
-        sys.exit() 
-
+threadRecieve.start()
+threadInput.start()
 
